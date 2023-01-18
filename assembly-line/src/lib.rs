4e5629aa1890595770e5f51de7df3bc675b53681
Gyen Abubakar <indehyde@gmail.com>
2023-01-18 22:24:46 +0000

    solved exercise: assembly-line

--- assembly-line/src/lib.rs
+++ assembly-line/src/lib.rs
@@ -2,10 +2,30 @@
 // to enable stricter warnings.
 #![allow(unused)]
 
+const CARS_PRODUCED_IN_HOUR_AT_SPEED_1: f64 = 221.0;
+
+enum SuccessRatesPerSpeedRange {
+    Range1To4,
+    Range5To8,
+    Range9To10,
+}
+
+fn get_success_rate(speed: u8) -> f64 {
+    if speed >= 1 && speed <= 4 {
+        1.0
+    } else if speed >= 5 && speed <= 8 {
+        0.9
+    } else {
+        0.77
+    }
+}
+
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    let cars_produced = (speed as f64) * CARS_PRODUCED_IN_HOUR_AT_SPEED_1;
+    get_success_rate(speed) * cars_produced
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    let production = production_rate_per_hour(speed) as u32;
+    production / 60 as u32
 }
