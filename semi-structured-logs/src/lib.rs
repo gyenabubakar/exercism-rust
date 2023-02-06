adcc6c496c2c81207b1ad7053220d70a808797a8
Gyen Abubakar <indehyde@gmail.com>
2023-02-06 07:48:26 +0000

    solved exercise: semi-structured-logs

--- semi-structured-logs/src/lib.rs
+++ semi-structured-logs/src/lib.rs
@@ -1,24 +1,26 @@
-// This stub file contains items that aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
 #![allow(unused)]
 
-/// various log levels
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub enum LogLevel {
     Info,
     Warning,
+    Debug,
     Error,
 }
-/// primary function for emitting logs
+
 pub fn log(level: LogLevel, message: &str) -> String {
-    unimplemented!("return a message for the given log level")
+    let level_str = format!("{:?}", level).to_uppercase();
+    format!("[{level_str}]: {message}")
 }
+
 pub fn info(message: &str) -> String {
-    unimplemented!("return a message for info log level")
+    log(LogLevel::Info, message)
 }
+
 pub fn warn(message: &str) -> String {
-    unimplemented!("return a message for warn log level")
+    log(LogLevel::Warning, message)
 }
+
 pub fn error(message: &str) -> String {
-    unimplemented!("return a message for error log level")
+    log(LogLevel::Error, message)
 }
